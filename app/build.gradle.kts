88738353ef1064478335fbe1752c582477bd107b
thseenk100-del <thseenk100@gmail.com>
2026-08-09 00:31:41 +0000

    Fix debug keystore path

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
       keyPassword = System.getenv("KEY_PASSWORD")
     }
     create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
+            storeFile = file("debug.keystore")
       storePassword = "android"
       keyAlias = "androiddebugkey"
       keyPassword = "android"
